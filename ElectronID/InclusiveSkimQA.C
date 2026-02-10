b5dd48a031ec94ddfe463c709b0e9fd18e2e938a
Win Lin <wl366@scarletmail.rutgers.edu>
2026-02-10 17:37:28 -0500

    missed to commit the loading lib for this example

--- ElectronID/InclusiveSkimQA.C
+++ ElectronID/InclusiveSkimQA.C
@@ -1,13 +1,6 @@
 // Find inclusive scattered electrons
 
-R__LOAD_LIBRARY(libpodio.so)
-R__LOAD_LIBRARY(libpodioRootIO.so)
-R__LOAD_LIBRARY(/opt/local/lib/libedm4hep.so)
-R__LOAD_LIBRARY(/opt/local/lib/libedm4hepDict.so)
-R__LOAD_LIBRARY(/opt/local/lib/libedm4hepUtils.so)
-R__LOAD_LIBRARY(/opt/local/lib/libedm4hepRDF.so)
-R__LOAD_LIBRARY(/opt/local/lib/libedm4eic.so)
-R__LOAD_LIBRARY(/opt/local/lib/libedm4eicDict.so)
+#include "preLoadLib.hh"
 
 #include "InclusiveSkimQA.h"
 
